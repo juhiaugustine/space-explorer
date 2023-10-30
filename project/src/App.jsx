77074c3ef58962882d25cb9cfd7be208cb471642
Akash Kumar <kumarak@pdx.edu>
2023-10-30 14:18:37 -0700

    added basic structure and Chatgpt feature

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,16 +1,18 @@
-import AstronautsInSpace from './components/current_astronauts'
-import './assets/css/App.css'
+import AstronautsInSpace from "./components/current_astronauts";
+import ChatGpt from "./components/chatgpt_feature";
+import "./assets/css/App.css";
 
 function App() {
-  
-
   return (
     <>
-    <div>
-      <AstronautsInSpace/>
-    </div>
+      <div>
+        <ChatGpt />
+      </div>
+      <div>
+        <AstronautsInSpace />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
