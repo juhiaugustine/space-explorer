93ed8b479da109729e010fd733e512ebc618eb44
shrutivd <svd@pdx.edu>
2023-11-08 21:50:07 -0800

    remaining vitest setup and first valid test

--- project/src/__tests_unit_/current_astronauts.test.jsx
+++ project/src/__tests_unit_/current_astronauts.test.jsx
@@ -1,6 +1,11 @@
 import {describe, it, expect} from "vitest"
 import TestRenderer from "react-test-renderer";
-import {AstronautsInSpace} from "../components/Current_Astronauts/current_astronauts";
+import AstronautsInSpace from "../components/Current_Astronauts/current_astronauts"
+import { render } from "@testing-library/react";
+
+/**
+* @vitest-environment jsdom
+*/
 
 
 
@@ -9,6 +14,16 @@ describe("example()", () => {
     it("checks if 5 is 5", () => {
         const num = 5;
         expect(num).toBe(5)
+    })
+})
+
+
+
+describe("AstronautsInSpace", () => {
+    it("dropdown has choose an astronaut option", () => {
+        const { getByText } = render(<AstronautsInSpace/>);
+        const chooseOption = getByText("Choose an astronaut");
+        expect(chooseOption).toBeInTheDocument();
     })
 })
 
