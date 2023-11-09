a69f41f1cfa25599c72c42d8259e15384fd851a7
shrutivd <svd@pdx.edu>
2023-11-08 16:51:24 -0800

    setup for vitest

---
+++ project/src/__tests_unit_/current_astronauts.test.jsx
@@ -0,0 +1,17 @@
+import {describe, it, expect} from "vitest"
+import TestRenderer from "react-test-renderer";
+import {AstronautsInSpace} from "../components/Current_Astronauts/current_astronauts";
+
+
+
+//basic test to check if vitest works
+describe("example()", () => {
+    it("checks if 5 is 5", () => {
+        const num = 5;
+        expect(num).toBe(5)
+    })
+})
+
+
+
+
