2380d0213896f09acecda19eba21dc7382434f0f
juhi <juh3@pdx.edu>
2023-11-09 21:58:47 -0800

    Revert "Added unit tests to MarsRoverImages component"
    This reverts commit adbb175422d6c8af53a34fb722283ed410cd65bc.

--- project/src/components/MarsRoverImages/MarsRoverImages.jsx
+++ project/src/components/MarsRoverImages/MarsRoverImages.jsx
@@ -1,63 +1,46 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MarsRoverImages.css';
-import React from 'react';
-
-export const API_KEY = 'zfpIuVyYaaKZETKqwLOst2vKrFubJaL4f0usFq7B'
-
-export const fetchRoverImages = async () => {
-  try {
-    const response = await axios.get(
-      'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',
-      {
-        params: {
-          sol: 1000,
-          api_key: API_KEY,
-          page: 1,
-          per_page: 10,
-        },
-      }
-    );
-    return response.data.photos;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
-};
 
 const MarsRoverImages = () => {
     const [images, setImages] = useState([]);
+
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const data = await fetchRoverImages();
-          setImages(data);
-        } catch (error) {
-          // Handle error
-        }
-      };
-  
-      fetchData();
+      axios
+        .get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos', {
+          params: {
+            sol: 1000,
+            api_key: 'zfpIuVyYaaKZETKqwLOst2vKrFubJaL4f0usFq7B',
+            page: 1,
+            per_page: 10,
+          },
+        })
+        .then((response) => {
+          setImages(response.data.photos);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
     }, []);
   
     return (
       <div className="gallery">
         <h1 className="heading">Mars Rover Photos</h1>
         <div className="horizontal-scroller">
-        {images.length > 0 && (
-        images.map((image, index) => (
-          <div className="image-container" key={index}>
-            <img
-              src={image.img_src}
-              alt={`Mars Rover ${index}`}
-              style={{ width: '200px', height: '200px' }}
-            />
-          </div>
-        ))
-      )}
+          {images.length > 0 && (
+            images.map((image, index) => (
+              <div className="image-container" key={index}>
+                <img
+                  src={image.img_src}
+                  alt={`Mars Rover ${index}`}
+                  style={{ width: '200px', height: '200px' }}
+                />
+              </div>
+            ))
+          )}
         </div>
       </div>
     );
   };
 
-  export default MarsRoverImages;
+export default MarsRoverImages;
