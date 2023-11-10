adbb175422d6c8af53a34fb722283ed410cd65bc
juhi <juh3@pdx.edu>
2023-11-09 21:52:00 -0800

    Added unit tests to MarsRoverImages component

---
+++ project/src/components/MarsRoverImages/MarsRoverImages.test.jsx
@@ -0,0 +1,16 @@
+import { expect, test, describe, it  } from 'vitest'
+import { API_KEY, MarsRoverImages } from './MarsRoverImages'
+import React from 'react';
+import {
+	render,
+} from "@testing-library/react";
+
+test('API Key check', () => {
+    expect(API_KEY, 'zfpIuVyYaaKZETKqwLOst2vKrFubJaL4f0usFq7B')
+})
+
+describe("MarsRoverImages", () => {
+	it("component renders without crashing", () => {
+		render(<MarsRoverImages />);
+	});
+})
