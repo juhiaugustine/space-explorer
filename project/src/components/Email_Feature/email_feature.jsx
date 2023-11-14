c03710c50ec571518f04a72c6d052babeed1dc2f
kumarakash101 <kumarak@pdx.edu>
2023-11-13 18:57:45 -0800

    Requested changes made also added a reset button

--- project/src/components/Email_Feature/email_feature.jsx
+++ project/src/components/Email_Feature/email_feature.jsx
@@ -1,5 +1,5 @@
 /** @format */
-
+import "./email_feature.css";
 import emailjs from "@emailjs/browser";
 import React, { useState } from "react";
 
@@ -40,15 +40,19 @@ export default function Contactus() {
     }
   }
   return (
-    <div>
+    <div className="body">
       <h2>Contact Us</h2>
+
       <form onSubmit={sendemail}>
         <div>
-          <label htmlFor="exampleFormControlInput1">Your Name:</label>
+          <label htmlFor="exampleFormControlInput1" className="labletext">
+            Your Name:
+          </label>
           <input
             type="text"
             id="exampleFormControlInput1"
             placeholder="Enter your name"
+            className="input"
             name="user_name"
             onChange={(e) =>
               setFirstName(e.target.value) && setnumber(e.target.value)
@@ -56,59 +60,83 @@ export default function Contactus() {
           />
 
           {error && Name.length <= 0 && Name.match(numbers) ? (
-            <label>Name is Required /</label>
+            <label>
+              <b>*Name is Required /</b>
+            </label>
           ) : (
             ""
           )}
           {error && Name.match(numbers) ? (
-            <label>Name cannot be a number</label>
+            <label>
+              <b>Name cannot be a number</b>
+            </label>
           ) : (
             ""
           )}
         </div>
         <div>
-          <label htmlFor="exampleFormControlInput2">Your Email address: </label>
+          <label htmlFor="exampleFormControlInput2" className="labletext">
+            Your Email address:{" "}
+          </label>
           <input
             type="email"
             id="exampleFormControlInput2"
+            className="input"
             placeholder="Enter your Email"
             name="user_emailaddress"
             onChange={(e) => setemail(e.target.value)}
           />
-          {error && uemail.length <= 0 ? <label>Email is Required</label> : ""}
+          {error && uemail.length <= 0 ? (
+            <label>
+              <b>*Email is Required</b>
+            </label>
+          ) : (
+            ""
+          )}
         </div>
 
         <div>
-          <label htmlFor="exampleFormControlTextarea1">Subject: </label>
+          <label htmlFor="exampleFormControlTextarea1" className="labletext">
+            Subject:{" "}
+          </label>
           <input
             type="text"
             id="exampleFormControlTextarea1"
             placeholder="Enter Subject"
+            className="input"
             name="user_subject"
             onChange={(e) => setsubject(e.target.value)}
           />
           {error && usubject.length <= 0 ? (
-            <label>Subject is Required</label>
+            <label>
+              <b>*Subject is Required</b>
+            </label>
           ) : (
             ""
           )}
         </div>
         <div>
-          <label htmlFor="exampleFormControlTextarea2">Your Message: </label>
+          <label htmlFor="exampleFormControlTextarea2" className="labletext">
+            Your Message:{" "}
+          </label>
           <textarea
             id="exampleFormControlTextarea2"
-            rows="3"
+            rows="4"
+            cols="40"
             placeholder="Enter your Message"
             name="user_message"
             onChange={(e) => setmessage(e.target.value)}
           ></textarea>
           {error && umessage.length <= 0 ? (
-            <label>Message is Required</label>
+            <label>
+              <b>*Message is Required</b>
+            </label>
           ) : (
             ""
           )}
         </div>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" className="btn" />
+        <input type="Reset" value="Reset" className="btn" />
       </form>
     </div>
   );
