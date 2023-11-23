057260f8a4d44e0e20ec62f93bc422f882bd2bed
juhi <juh3@pdx.edu>
2023-11-22 18:56:19 -0800

    Re-structured components in the website

--- project/src/components/Home/Home.jsx
+++ project/src/components/Home/Home.jsx
@@ -4,12 +4,15 @@ import blankImage from "../../assets/images/blankImage.png";
 import AstronautsInSpace from "../Current_Astronauts/current_astronauts";
 import ChatGpt from "../ChatGPT_Feature/chatgpt_feature";
 import MarsRoverImages from "../MarsRoverImages/MarsRoverImages";
-import Contactus from "../Email_Feature/email_feature";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import { Link } from "react-router-dom";
 
 function Home() {
+  const centerStyle = {
+    textAlign: 'center', // Aligns the text content to the center
+    color: 'white', // Sets the text color to white
+  };
   const [imageOfDay, setImageOfDay] = useState([]); 
 
   useEffect(() => {
@@ -25,17 +28,42 @@ function Home() {
 
   return (
     <div className="background-container">
-      {/*  Background image */}
+      {/* Background image */}
       <div className="background-image"></div>
-      <MarsRoverImages />
-      {/* Image of the day */}
-      <div className="image-placeholder">
-        <img src={imageOfDay.url} alt={imageOfDay.title} />
+
+      {/* Scrollable container for components */}
+      <div className="scrollable-components">
+        {/* Mars Rover Images */}
+        <MarsRoverImages />
+
+        {/* Other components */}
+        <div className="column">
+          <div className="bottom-components">
+
+            <div className="bottom-components-container">
+              <ChatGpt />
+            </div>
+
+            <div className="bottom-components-container">
+            <h1 className="subHeading"> Astronomy Picture of the Day </h1>
+              <div className="image-placeholder">
+                <img src={blankImage} alt={imageOfDay.title} />
+              </div>
+            </div>
+
+
+            <div className="bottom-components-container">
+              <AstronautsInSpace />
+            </div>
+          </div>
+            
+        </div>
+        <p  style={centerStyle}>
+        Have a question? <Link to="/contactus" style={{ color: 'white' }}>Reach out</Link>.
+      </p>
       </div>
-      <ChatGpt />
-      <Contactus />
-      <AstronautsInSpace />
     </div>
+     
   );
 }
 export default Home;
--- project/src/main.jsx
+++ project/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './components/Home/Home.jsx'
+import Home from './components/Home/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ContactUs from './components/Email_Feature/email_feature';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+   <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/contactus" element={<ContactUs/>} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
